bb746c01d5051b2b4bcc595454b3bfa83748d9d8
OlSerkes <serkes@i.ua>
2021-04-29 22:56:53 +0300

    Added filtres by size and categories

--- index.js
+++ index.js
@@ -77,22 +77,49 @@ const mockData = [{
     quantity: 0,
     discount: 0.35,
     sizes: [40, 44, 48],
-    image: "img/p8.jpg" 
+    image: "img/p8.jpg"
 }]
-const data = [...mockData];
+let data = [];
+
+const updateData = (newData) => {
+    data = [...newData];
+    const productContainer = document.querySelector(".product-list");
+    renderList(productContainer);
+}
+
+const filterInStock = (event) => {
+    const checked = event.target.checked;
+    const newData = mockData.filter((product) => {
+        if (checked) {
+            return product.quantity > 0;
+        }
+        return true;
+    });
+    updateData(newData);
+}
+const filterByCategory = (event) => {
+    const category = event.target.value;
+    const newData = mockData.filter((product) => {
+        return category === "all" || product.category === category;
+    });
+    updateData(newData);
+}
 const renderList = (container) => {
-        const html = `<div class="products-container">` + data.reduce((html, product) => {
+    const html = `<div class="products-container">` + data.reduce((html, product) => {
         return html + `
 <div class="product-wrapper">
 <img src="${product.image}" alt="${product.name}" class="product-image">
 <div class="product-name">${product.name}</div>
 <div class="product-price-wrapper">
+<div class="product-price-line"> 
 <div class="product-price">
-${product.price*(1 - product.discount)}
+$${product.price * (1 - product.discount)}
 </div>
 <div class="product-old-price">
-${product.price}
+$${product.price}
 </div>
+</div>
+<div class="button">Add to cart</div>
 
 
 
@@ -102,13 +129,55 @@ ${product.price}
     }, "") + `</div>`;
     container.innerHTML = html
 }
-
-const renderFiltres = () => {
-
+const getCategories = (data) => {
+    return data.reduce((categories, product) => {
+        if (!categories.includes(product.category)) {
+            categories.push(product.category);
+        }
+        return categories;
+    }, []);
+}
+const getSizes = (data) => {
+    const allSizes = data.reduce((sizes, product) => {
+        return [...sizes, ...product.sizes];
+    }, []);
+    return [...new Set(allSizes)].sort();
+}
+const renderBase = (container) => {
+    const categories = getCategories(mockData);
+    const sizes = getSizes(mockData);
+    container.innerHTML = `
+<div class="filters-wrapper">
+<label>
+<input type="checkbox" id="instock-checkbox">
+Только в наличии
+</label>
+<select id="category-filter">
+<option value="all">All categories</option>
+${categories.map((category) => (`
+<option value="${category}">${category}</option>
+`)).join("")}
+</select>
+<div class="filter-checkboxes">
+Размеры:
+${sizes.map((size) => (`
+<p>
+<label for=""><input type="checkbox" value="${size}">${size}</label>
+</p>
+`)).join("")}
+</div>
+</div>
+<div class="product-list"></div>
+`;
+    const inStockCheckbox = document.querySelector("#instock-checkbox");
+    inStockCheckbox.addEventListener("change", filterInStock);
+    const categoryFilter = document.querySelector("#category-filter");
+    categoryFilter.addEventListener("change", filterByCategory);
 }
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.querySelector(".container");
-    renderList(container);
+    renderBase(container);
+    updateData(mockData);
 })
 
 
